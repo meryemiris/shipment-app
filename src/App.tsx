163ac8b9fa56c7f458a7c38c914f246a98d41237
Meryem İris <irismeryem.96@gmail.com>
2024-01-04 14:54:27 +0300

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <ShipmentTable shipments={[]} />,
+    element: <ShipmentTable />,
   },
   {
     path: "/details/:orderNo",
